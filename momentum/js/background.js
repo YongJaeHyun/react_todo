baab3e4e43c94c7429de5cf46b40a80977ac6a02
YongjaeHyun <dltjrrbs2020@gmail.com>
2022-04-05 14:06:15 +0900

    배경화면 뒤로가기버튼 작업 중 공유

--- momentum/js/background.js
+++ momentum/js/background.js
@@ -1,6 +1,8 @@
-const bgButton = document.querySelector("#change-background-btn");
+const changeBgButton = document.querySelector("#change-background-btn");
+const prevBgButton = document.querySelector("#prev-background-btn")
 const BACKGROUND = "background";
 
+let prevBgImage = []
 const bgImage = [
   "0.jpg",
   "1.jpg",
@@ -22,16 +24,32 @@ function setBgImage(img) {
   bgMain.backgroundImage = `linear-gradient(rgba(0,0,0,0.2), rgba(0,0,0,0.2)), url("img/${img}.jpg")`;
 }
 
-function changeBgImage() {
-  const chosenImg = Math.floor(Math.random() * bgImage.length);
-  setBgImage(chosenImg);
-  localStorage.setItem(BACKGROUND, chosenImg);
+function changeBgImage(e) {
+  e.preventDefault()
+  const changedImg = Math.floor(Math.random() * bgImage.length);
+  setBgImage(changedImg);
+  prevBgImage.push(changedImg)
+  console.log(prevBgImage)
+  localStorage.setItem(BACKGROUND, changedImg);
+}
+
+function backBgImage(e) {
+  e.preventDefault()
+  const isSame = prevBgImage == prevBgImage.slice(0,-1)
+  const prevBgImage2 = isSame ? prevBgImage : prevBgImage.slice(0,-1)
+  const changedImg = prevBgImage2.pop()
+  setBgImage(changedImg)
+  console.log(prevBgImage2)
+  localStorage.setItem(BACKGROUND, changedImg)
 }
 
 const savedImg = localStorage.getItem(BACKGROUND);
 if (savedImg) {
   setBgImage(savedImg);
+  prevBgImage.push(savedImg)
+  console.log(prevBgImage)
 } else {
   changeBgImage();
 }
-bgButton.addEventListener("click", changeBgImage);
+changeBgButton.addEventListener("click", changeBgImage);
+prevBgButton.addEventListener("click", backBgImage)
